0d13ec21f261d463fc84214b64a4eff92d51aa3b
Robert Kolner <robert.kolner@gmail.com>
2013-08-21 21:05:18 +0200

    Improving User-view in admin interface.

--- dusken/admin.py
+++ dusken/admin.py
@@ -3,15 +3,7 @@ from django.contrib.auth.models import User
 from dusken.models import Member, Institution
 
 class MemberAdmin(admin.ModelAdmin):
-    fieldsets = [
-        ('Other User instances',
-            {'fields' : ['legacy_id']}),
-        ('User data',
-            {'fields' : ['username', 'email', 'first_name', 'last_name']}),
-        ('User data',
-            {'fields' : ['phone_number', 'date_of_birth', 'address', 'place_of_study']}),
-    ]
-
+    exclude = ( 'password', 'last_login', )
     readonly_fields = ( 'username', )
 
 class InstitutionAdmin(admin.ModelAdmin):
