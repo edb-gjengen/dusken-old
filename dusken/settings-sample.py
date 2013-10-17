320e10dbbbc597add75f31e6e2db3a9dc7a67fc0
Nikolai Kristiansen <nikolaik@gmail.com>
2013-10-17 10:46:07 +0200

    Travis CI postgresql

--- dusken/settings-sample.py
+++ dusken/settings-sample.py
@@ -12,10 +12,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'main',                      # Or path to database file if using sqlite3.
-        'USER': 'mdb_su',                      # Not used with sqlite3.
+        'NAME': 'dusken_test',                      # Or path to database file if using sqlite3.
+        'USER': 'postgres',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': 'postgresql.neuf.no',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
